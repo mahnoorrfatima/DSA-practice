319a685be9cd24e6df52bb0b472aa82ca390224b
Mahnoor <154813449+mahnoorrfatima@users.noreply.github.com>
2026-08-14 21:24:57 +0500

    Update Two-sum.py

--- Two-sum.py
+++ Two-sum.py
@@ -1,1 +1,21 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen={}
+
+        for i in range(len(nums)):
+          need = target - nums[i]
+
+          if need in seen:
+            return [seen[need], i] 
+
+          seen[nums[i]] = i 
+
+
+#Complexity
+#Time: O(n) — we go through the array once.
+#Space: O(n) — in the worst case, we store many elements in seen.
+
+
+
+
 
