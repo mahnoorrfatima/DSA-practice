bb18b15032f4e9213636da10b8efd07c5ac7a43d
Mahnoor <154813449+mahnoorrfatima@users.noreply.github.com>
2026-08-14 22:10:28 +0500

    Create group-anagrams.py

---
+++ group-anagrams.py
@@ -0,0 +1,21 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+      anagrams={}
+
+      for s in strs:
+        key="".join(sorted(s))
+
+        if key not in anagrams:
+          anagrams[key]= []
+
+        anagrams[key].append(s) 
+
+      return list(anagrams.values())
+
+#TIME COMPLEXITIES 
+#sorting: O(k log k)
+#sorting for n strings: n x O(k log k) = 0(n. k log k) 
+
+#SPACE:
+#O(n . k)  = O(nk) 
+
